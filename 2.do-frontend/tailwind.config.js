4e9c848b00278da8b51feb897160f36c59bf9ddd
Matthew Latham <50916823+latham7@users.noreply.github.com>
2022-11-06 21:00:04 -0500

    install FastAPI & Added /ping route

--- 2.do-frontend/tailwind.config.js
+++ 2.do-frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontSize: {
+      'xs': '.75rem',
+    },
     extend: {},
   },
   plugins: [],
