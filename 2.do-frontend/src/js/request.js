ed2b7357f9055fc4850078ea2a5dfbe498a995ee
Matthew Latham <50916823+latham7@users.noreply.github.com>
2022-11-07 20:02:50 -0500

    cleaned and tweaked reponse

--- 2.do-frontend/src/js/request.js
+++ 2.do-frontend/src/js/request.js
@@ -1,33 +1,14 @@
-const instance = axios.create({
-    baseURL: 'https://127.0.0.1:8000/',
-    timeout: 1000,
-  });
 
 
+// set config for Axios
 const api = axios.create({
     baseURL: 'http://localhost:8000',
-//     headers: {
-//         "Access-Control-Allow-Origin": "*",
-// }
  });
 
 
 function makeRequest2() {
     api.get('/ping').then(response => {
-        document.getElementById('response').innerHTML = response.data;
-        console.log(response);
-    });
-}
-
-// axios make request and replace html content
-function makeRequest() {
-    return axios({
-        method: "get",
-        url: "/ping",
-    }).then(function (response) {
-        document.getElementById('response').innerHTML = response.data;
-        console.log(response);
-    }).catch(function (error) {
-        console.log(error);
+        document.getElementById('response').innerHTML = response.data.ping;
+        console.log(response.data);
     });
 }
